dfca328bd1f95f5e0e2507ef228eb664e8090181
yamaneco <ykouki111228@gmail.com>
2020-06-30 23:17:14 +0900

    Support for different domains

--- 5chScrape.py
+++ 5chScrape.py
@@ -6,44 +6,30 @@ import json
 import re
 import datetime
 
+from gdrive import *
 
-def writeGDrive(drive, title, parentId, string):
-  f = drive.CreateFile({'title': title, 'parents': [{'id': parentId}]})
-  f.SetContentString(string)
-  f.Upload()
 
-def getFolderIDOnGDrive(drive, folderName, parentId='root'):
-  fileList = drive.ListFile({
-    'q': 'title = "' + folderName + '" and' +
-         'mimeType = "application/vnd.google-apps.folder" and' +
-         'trashed=false and' +
-         '"{}" in parents'.format(parentId),
-    'maxResults': 100,
-  }).GetList()
-
-  if len(fileList) == 0:
-    folder = drive.CreateFile({'title': folderName,
-                               'mimeType': 'application/vnd.google-apps.folder',
-                               'parents': [{'id': parentId}]})
-    folder.Upload()
-    return folder['id']
-
-  return fileList[0]['id']
+def getDomain(url):
+  return re.match(r'(?:https?://)?(?P<host>.*?)(?:[:#?/@]|$)', url).group(0)
 
 def getLinksFromBoard(url):
   html = requests.get(url)
   soup = BeautifulSoup(html.content, 'html5lib')
   board = soup.find(class_='board')
   if board == None: return []
-  return ['https://1999.5ch.net' + link.get('href') for link in board.find_all('a')]
+
+  domain = getDomain(url)
+  return [domain[:-1] + link.get('href') for link in board.find_all('a')]
 
 def getLinksFromMain(url):
+  domain = getDomain(url)
+
   html = requests.get(url)
   soup = BeautifulSoup(html.content, 'html5lib')
 
   boardName = soup.find(class_='name_strings')
-  tag = ''
-  if boardName != None: tag = boardName.text
+  board = ''
+  if boardName != None: board = boardName.text
 
   threads = soup.find(class_='main')
   if threads == None: return []
@@ -55,13 +41,17 @@ def getLinksFromMain(url):
     if int(lines.text) < 100: continue
     link = p.find('a')
     if link == None: continue
-    links.append('https://1999.5ch.net' + link.get('href'))
-  return tag, links
+    links.append(domain + link.get('href'))
+  return board, links
 
 def getDatetime(text):
-  text_date = re.search(r'\d\d\d\d\/\d\d/\d\d', text).group(0)
-  text_time = re.search(r'\d\d:\d\d', text).group(0)
-  d = datetime.datetime.strptime(text_date + ' ' + text_time, '%Y/%m/%d %H:%M')
+  match_date = re.search(r'\d\d\/\d\d/\d\d', text)
+  if match_date != None: text_date = match_date.group(0)
+  else: return ''
+  match_time = re.search(r'\d\d:\d\d', text)
+  if match_time != None: text_time = match_time.group(0)
+  else: return ''
+  d = datetime.datetime.strptime(text_date + ' ' + text_time, '%y/%m/%d %H:%M')
   return d.strftime('%Y-%m-%d %H:%M:%S')
 
 def getThread(url, boardName):
@@ -82,6 +72,7 @@ def getThread(url, boardName):
     'title': title,
     'datetime': getDatetime(thread.find('dt').text),
     'board': boardName,
+    'url': url,
     'content': posts
     }
 
@@ -99,35 +90,23 @@ def main():
 
   drive = GoogleDrive(gauth)
 
-  folderId_5chScrape = getFolderIDOnGDrive(drive, '5chScrape')
-
-  folderIdList = {}
-  for year in range(1999, 2021):
-    folderIdList[str(year)] = getFolderIDOnGDrive(drive, str(year), parentId=folderId_5chScrape)
-    for month in range(1, 13):
-      folderName = str(year) + str(month).zfill(2)
-      folderIdList[folderName] = getFolderIDOnGDrive(drive, folderName, parentId=folderIdList[str(year)])
-      print('get folderId: ' + folderName)
-  print(folderIdList)
+  folderIdList = getFolderIdList('folderIDList.json', drive)
 
   urlName = 'http://lavender.5ch.net/kakolog_servers.html'
   url = requests.get(urlName)
   soup = BeautifulSoup(url.content, 'html5lib')
-  links = [link.get('href') for link in soup.find_all('a')][:1]
+  links = [link.get('href') for link in soup.find_all('a')]
 
-  links_board = sum([getLinksFromBoard(link) for link in links], [])[5:]
-  # i = 0
-  for link_board in links_board:
-    tag, links_thread = getLinksFromMain(link_board)
-
-    for link_thread in links_thread:
-      thread = getThread(link_thread, tag)
-      if thread == None: continue
-      print('get thread data from ' + link_thread)
-      json_thread = json.dumps(thread, ensure_ascii=False, indent=2)
-      folderId = folderIdList[getFolderName(thread)]
-      writeGDrive(drive, makeFilename(thread), folderId, json_thread)
-      # i += 1
-      # if i > 1000: break
+  for link in links:
+    links_board = getLinksFromBoard(link)
+    for link_board in links_board:
+      board, links_thread = getLinksFromMain(link_board)
+      for link_thread in links_thread:
+        thread = getThread(link_thread, board)
+        if thread == None: continue
+        print('get thread data: {} {} {} {}'.format(thread['board'], thread['datetime'], link_thread, thread['title']))
+        json_thread = json.dumps(thread, ensure_ascii=False, indent=2)
+        folderId = folderIdList[getFolderName(thread)]
+        writeGDrive(drive, makeFilename(thread), folderId, json_thread)
 
 if __name__ == '__main__': main()
---
+++ gdrive.py
@@ -0,0 +1,57 @@
+from pydrive.drive import GoogleDrive
+from pydrive.auth import GoogleAuth
+import json
+import os
+
+
+def writeGDrive(drive, title, parentId, string):
+  fileList = drive.ListFile({
+    'q': "title = '{}' and '{}' in parents and trashed=false".format(title, parentId)
+  }).GetList()
+
+  if len(fileList) == 0:
+    f = drive.CreateFile({'title': title, 'parents': [{'id':parentId}]})
+  else: f = fileList[0]
+
+  f.SetContentString(string)
+  f.Upload()
+
+def getFolderIDOnGDrive(drive, folderName, parentId='root'):
+  fileList = drive.ListFile({
+    'q': 'title = "' + folderName + '" and' +
+         'mimeType = "application/vnd.google-apps.folder" and' +
+         'trashed=false and' +
+         '"{}" in parents'.format(parentId),
+    'maxResults': 100,
+  }).GetList()
+
+  if len(fileList) == 0:
+    folder = drive.CreateFile({'title': folderName,
+                               'mimeType': 'application/vnd.google-apps.folder',
+                               'parents': [{'id': parentId}]})
+    folder.Upload()
+    return folder['id']
+
+  return fileList[0]['id']
+
+def getFolderIdList(filename, drive):
+  if os.path.exists(filename):
+    folderIdListFile = open(filename, 'r')
+    folderIdList = json.load(folderIdListFile)
+    return folderIdList
+
+  folderId_5chScrape = getFolderIDOnGDrive(drive, '5chScrape')
+
+  folderIdList = {}
+  for year in range(1999, 2021):
+    folderIdList[str(year)] = getFolderIDOnGDrive(drive, str(year), parentId=folderId_5chScrape)
+    for month in range(1, 13):
+      folderName = str(year) + str(month).zfill(2)
+      folderIdList[folderName] = getFolderIDOnGDrive(drive, folderName, parentId=folderIdList[str(year)])
+      print('get folderId: ' + folderName)
+  print(folderIdList)
+  json_folderIdList = json.dumps(folderIdList, ensure_ascii=False, indent=2)
+  with open(filename, mode='w') as f:
+    f.write(json_folderIdList)
+
+  return folderIdList
